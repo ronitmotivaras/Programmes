6e9801ca42ea1103a7c38d62ecbcd73ec9fa71e4
Ronit Motivaras <ronitmotivaras04@gmail.com>
2025-11-28 15:47:30 +0530

    add JS Prohrammes

---
+++ JS/vaiables.js
@@ -0,0 +1,9 @@
+const accountId = 144553
+let accountName = "ronit"
+var accountPassword = "12345" //var is old way because of scope
+accountCity = "ahm"
+let accountState;
+
+console.log(accountId);
+
+console.table([accountId, accountName, accountPassword, accountCity,accountState])
